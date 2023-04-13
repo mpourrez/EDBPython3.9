614ddec69076bfb530d7f542c2aba251b443e846
Maryam Pourreza <mpourrez@andrew.cmu.edu>
2023-04-13 13:39:29 -0700

    added object_detection

---
+++ applications/object_detection_darknet_gpu.py
@@ -0,0 +1,49 @@
+from utils import current_milli_time
+from protos import benchmark_pb2 as pb2
+import base64
+import os
+import subprocess
+
+def detect(request, request_received_time_ms):
+    # Decode base64 string to binary format
+    binary_data = base64.b64decode(request.image)
+
+    # Save binary data to a file
+    with open('input.jpg', 'wb') as f:
+        f.write(binary_data)
+
+    # Use Darknet to perform object classification with GPU
+    cmd = ['darknet', 'detector', 'test', 'cfg/coco.data', 'cfg/yolov3.cfg', 'yolov3.weights', '-dont_show',
+           'input.jpg']
+    env = os.environ.copy()
+    env['CUDA_VISIBLE_DEVICES'] = '0'  # Set the GPU index here
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+    output, err = proc.communicate()
+
+    # Parse the output of Darknet and return the results
+    results = []
+    detected_objects = []
+    lines = output.decode().split('\n')
+    for line in lines:
+        if line.startswith('Enter Image Path:'):
+            continue
+        if not line.strip():
+            continue
+        if '%' in line:
+            continue
+        if ':' in line:
+            parts = line.split(':')
+            label = parts[0].strip()
+            confidence = float(parts[1].strip().replace('%', '')) / 100.0
+            results.append((label, confidence))
+            detected_objects = pb2.DetectedTrackedObject()
+            detected_objects.clazz = label
+            detected_objects.append(detected_objects)
+
+    detection_result = pb2.ObjectDetectionResponse()
+    detection_result.request_time_ms = request.request_time_ms
+    detection_result.request_received_time_ms = request_received_time_ms
+    detection_result.response_time_ms = current_milli_time()
+    detection_result.detected_objects.extend(detected_objects)
+    print("[x] Object-detection - Responded the client request")
+    return detection_result
