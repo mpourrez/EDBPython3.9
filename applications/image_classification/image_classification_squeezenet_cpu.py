1217a78f523aaefbbeb3f92304a32d35d7ae8d4c
Maryam Pourreza <mpourrez@andrew.cmu.edu>
2023-05-16 11:06:57 -0700

    update image classification squeezenet model loading

--- applications/image_classification/image_classification_squeezenet_cpu.py
+++ applications/image_classification/image_classification_squeezenet_cpu.py
@@ -1,4 +1,5 @@
 from utils import current_milli_time
+from applications.image_classification.squeezenet_module import SqueezeNet
 from protos import benchmark_pb2 as pb2
 import base64
 from io import BytesIO
@@ -7,6 +8,9 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.models as models
+
+squeezenet = models.squeezenet1_0(pretrained=True)
+torch.save(squeezenet.state_dict(), 'squeezenet.pth')
 
 
 def classify_image(request, request_received_time_ms):
@@ -21,7 +25,7 @@ def classify_image(request, request_received_time_ms):
     ])
 
     device = torch.device('cpu')
-    model = SqueezeNet(num_classes=2)
+    model = SqueezeNet(num_classes=1000)
     model.transform = transform
     model.load_state_dict(torch.load('squeezenet.pth', map_location=device))
     model.to(device)
@@ -31,8 +35,8 @@ def classify_image(request, request_received_time_ms):
         prob, predicted = torch.max(output, 1)
 
         classification_response = pb2.ImageClassificationResponse()
-        classification_response.top_category_id = predicted
-        classification_response.top_category_probability = prob * 100
+        classification_response.top_category_id = int(predicted.item())
+        classification_response.top_category_probability = int(prob.item() * 100)
         classification_response.request_time_ms = request.request_time_ms
         classification_response.request_received_time_ms = request_received_time_ms
         classification_response.response_time_ms = current_milli_time()
