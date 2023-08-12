8c5f7ea32c7f7a6125acc73f9af7f0dbd75dfb04
Maryam Pourreza <mpourrez@andrew.cmu.edu>
2023-08-12 11:49:38 -0700

    Updated FFT response

--- applications/micro_benchmarks/fast_fourier_transform.py
+++ applications/micro_benchmarks/fast_fourier_transform.py
@@ -9,7 +9,7 @@ def transform(request, request_received_time_ms):
     for i in range(25):
         fourier_result = np.fft.fft(fourier_input)
 
-    fourier_response = pb2.FastFourierResponse(fourier_output=pb2.Row(values=list(fourier_result)))
+    fourier_response = pb2.FastFourierResponse(fourier_output=pb2.Row(values=np.real(list(fourier_result))))
     fourier_response.request_time_ms = request.request_time_ms
     fourier_response.request_received_time_ms = request_received_time_ms
     fourier_response.response_time_ms = current_milli_time()
