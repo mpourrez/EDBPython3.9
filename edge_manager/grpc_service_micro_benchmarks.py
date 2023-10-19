fcf6464187e3fef5fe2e7e3a269e9482f6cd8ad8
Maryam Pourreza <mpourrez@andrew.cmu.edu>
2023-10-19 10:39:35 -0700

    Added all microbenchmarks

--- edge_manager/grpc_service_micro_benchmarks.py
+++ edge_manager/grpc_service_micro_benchmarks.py
@@ -1,7 +1,10 @@
 from protos import benchmark_pb2_grpc as pb2_grpc
 
-from applications.micro_benchmarks import fast_fourier_transform
+from applications.micro_benchmarks import floating_point_sqrt, floating_point_sin, \
+    fast_fourier_transform, sorter, dd_cmd, iperf3
 from utils import current_milli_time
+
+import multiprocessing
 
 
 class MicroBenchmarksGRPCService(pb2_grpc.MicroBenchmarksServicer):
@@ -11,3 +14,27 @@ class MicroBenchmarksGRPCService(pb2_grpc.MicroBenchmarksServicer):
         fourier_response = fast_fourier_transform.transform(request, request_received_time_ms)
         return fourier_response
 
+    def floating_point_sqrt(self, request, context):
+        request_received_time_ms = current_milli_time()
+        sqrt_response = floating_point_sqrt.sqrt(request, request_received_time_ms)
+        return sqrt_response
+
+    def floating_point_sin(self, request, context):
+        request_received_time_ms = current_milli_time()
+        sine_response = floating_point_sin.sine(request, request_received_time_ms)
+        return sine_response
+
+    def sort_file(self, request, context):
+        request_received_time_ms = current_milli_time()
+        sort_response = sorter.sort(request, request_received_time_ms)
+        return sort_response
+
+    def dd_cmd(self, request, context):
+        request_received_time_ms = current_milli_time()
+        dd_response = dd_cmd.dd(request, request_received_time_ms)
+        return dd_response
+
+    def run_iperf(self, request, context):
+        request_received_time_ms = current_milli_time()
+        iperf_response = iperf3.run_iperf(request, request_received_time_ms)
+        return iperf_response
